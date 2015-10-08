d5c698a6cd2d5f8fb524f8a13ca4f385ed629855
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-08 22:36:35 +0100

    Added constructor

--- spec/Domain/Payment/PaymentSpec.php
+++ spec/Domain/Payment/PaymentSpec.php
@@ -3,6 +3,7 @@
 namespace spec\CubicMushroom\Payments\Stripe\Domain\Payment;
 
 use CubicMushroom\Payments\Stripe\Domain\Payment\Payment;
+use Money\Money;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -12,12 +13,19 @@ use Prophecy\Argument;
  *
  * @package CubicMushroom\Payments\Stripe
  *
- * @see \CubicMushroom\Payments\Stripe\Domain\Payment\Payment
+ * @see     \CubicMushroom\Payments\Stripe\Domain\Payment\Payment
  */
 class PaymentSpec extends ObjectBehavior
 {
+    public function let(Money $cost)
+    {
+        // @todo - Make $token into a value object
+        $this->beConstructedWith($cost, $token = 'ugcashdcial');
+    }
+
+
     /**
-     * @uses Payment
+     * @uses Payment::__construct()
      */
     function it_is_initializable()
     {
--- src/Domain/Payment/Payment.php
+++ src/Domain/Payment/Payment.php
@@ -2,6 +2,24 @@
 
 namespace CubicMushroom\Payments\Stripe\Domain\Payment;
 
+use Money\Money;
+
+
+/**
+ * Class Payment
+ *
+ * @package CubicMushroom\Payments\Stripe
+ *
+ * @see     \spec\CubicMushroom\Payments\Stripe\Domain\Payment\PaymentSpec
+ */
 class Payment
 {
+    /**
+     * @param Money  $cost Amount & Currency of payment
+     * @param string $token  Token passed from Stripe.js
+     */
+    public function __construct(Money $cost, $token)
+    {
+        // TODO: write logic here
+    }
 }
