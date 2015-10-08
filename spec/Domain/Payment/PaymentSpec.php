e6b5c9b3f20c516dbbf322a9a15a0a17a2f08821
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-08 22:45:23 +0100

    Returning Cost/Amount/Currency

--- spec/Domain/Payment/PaymentSpec.php
+++ spec/Domain/Payment/PaymentSpec.php
@@ -3,6 +3,7 @@
 namespace spec\CubicMushroom\Payments\Stripe\Domain\Payment;
 
 use CubicMushroom\Payments\Stripe\Domain\Payment\Payment;
+use Money\Currency;
 use Money\Money;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -20,6 +21,8 @@ class PaymentSpec extends ObjectBehavior
     public function let(Money $cost)
     {
         // @todo - Make $token into a value object
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        /** @noinspection SpellCheckingInspection */
         $this->beConstructedWith($cost, $token = 'ugcashdcial');
     }
 
@@ -30,6 +33,34 @@ class PaymentSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(Payment::class);
+    }
+
+
+    /**
+     * @uses Payment::__construct()
+     * @uses Payment::getAmount()
+     * @uses Payment::getCurrency()
+     * @uses Payment::getCost()
+     */
+    function it_returns_the_amount_and_currency(
+        /** @noinspection PhpDocSignatureInspection */
+        Money $cost, Currency $currency)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $cost->getAmount()->willReturn(999);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $cost->getCurrency()->willReturn($currency);
+
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        /** @noinspection SpellCheckingInspection */
+        $this->beConstructedWith($cost, 'hiahicdscd');
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->getAmount()->shouldReturn(999);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->getCurrency()->shouldReturn($currency);
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->getCost()->shouldReturn($cost);
     }
 
 
--- src/Domain/Payment/Payment.php
+++ src/Domain/Payment/Payment.php
@@ -2,6 +2,7 @@
 
 namespace CubicMushroom\Payments\Stripe\Domain\Payment;
 
+use Money\Currency;
 use Money\Money;
 
 
@@ -15,11 +16,48 @@ use Money\Money;
 class Payment
 {
     /**
-     * @param Money  $cost Amount & Currency of payment
-     * @param string $token  Token passed from Stripe.js
+     * @var Money
+     */
+    protected $cost;
+
+
+    /**
+     * @param Money  $cost  Amount & Currency of payment
+     * @param string $token Token passed from Stripe.js
      */
     public function __construct(Money $cost, $token)
     {
-        // TODO: write logic here
+        $this->cost = $cost;
+    }
+
+
+    /**
+     * Returns the amount of the payment a an integer
+     *
+     * @return int
+     */
+    public function getAmount()
+    {
+        return $this->cost->getAmount();
+    }
+
+
+    /**
+     * Returns the currency part of the $cost property
+     *
+     * @return Currency
+     */
+    public function getCurrency()
+    {
+        return $this->cost->getCurrency();
+    }
+
+
+    /**
+     * @return Money
+     */
+    public function getCost()
+    {
+        return $this->cost;
     }
 }
