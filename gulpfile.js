9ebf9defe20645c115124d529e9affbc8aba1464
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-10 16:10:15 +0100

    Now using gulp-cm-phpspec-tasks for gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,5 @@
 var gulp    = require('gulp'),
-    notify  = require('gulp-notify'),
-    phpspec = require('gulp-phpspec');
+  phpspecTasks = require("gulp-cm-phpspec-tasks");
 
-var phpspecGlob = 'spec/**/*Spec.php';
-var srcGlob = 'src/**/*.php';
 
-gulp.task('phpspec', function () {
-  var options = {debug: true, verbose: 'v', notify: true};
-
-  gulp.src(phpspecGlob)
-    .pipe(phpspec('', options))
-    .on('error', notify.onError({
-      title  : "Testing Failed",
-      message: "Error(s) occurred during PHPSpec test..."
-    }));
-});
-
-gulp.task('watch', function () {
-  gulp.watch([phpspecGlob, srcGlob], ['phpspec']);
-});
+phpspecTasks.addTasks(gulp, 'CubicMushroom\\Payments\\Stripe\\',{bin: 'vendor/bin/phpspec --ignore-pending'});
