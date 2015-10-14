720e833fdf8d9a6030bb9a7fffda072eeea6630d
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-14 09:55:32 +0100

    Added test for validation errors on empty TakePaymentCommand

---
+++ tests/integration/Command/Payment/EmpyTakePaymentCommandFailsValidationCept.php
@@ -0,0 +1,18 @@
+<?php
+use CubicMushroom\Payments\Stripe\Command\Payment\TakePaymentCommand;
+use CubicMushroom\Payments\Stripe\Step\Integration\CommandValidationWizard;
+
+/** @var \Codeception\Scenario $scenario */
+$I = new CommandValidationWizard($scenario);
+$I->wantTo('check an empty take payment command does not validate');
+
+$command = new TakePaymentCommand();
+$I->validateCommand($command);
+$I->expectTheFollowingValidationErrors(
+    [
+        'cost'        => ['Please provide the cost details'],
+        'token'       => ['Please provide a payment card'],
+        'description' => ['Please provide a description of the payment'],
+        'userEmail'   => ['Please provide the user\'s email address'],
+    ]
+);
