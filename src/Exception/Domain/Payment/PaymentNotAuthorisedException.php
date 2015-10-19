8131fc4f19cd3b59fe864b456847ef360f3ff920
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-19 13:17:09 +0100

    Made PaymentFailed exception publicly safe

--- src/Exception/Domain/Payment/PaymentNotAuthorisedException.php
+++ src/Exception/Domain/Payment/PaymentNotAuthorisedException.php
@@ -8,11 +8,25 @@
 
 namespace CubicMushroom\Payments\Stripe\Exception\Domain\Payment;
 
+use CubicMushroom\Payments\Stripe\Exception\PublicSafeMessageInterface;
+
 /**
  * Exception thrown when a payment is rejected by the gateway
  *
+ * IMPORTANT NOTE…
+ * The message on this class should only ever bet set to the Stripe response error for failed payments
+ *
  * @package CubicMushroom\Payments
  */
-class PaymentNotAuthorisedException extends AbstractPaymentException
+class PaymentNotAuthorisedException extends AbstractPaymentException implements PublicSafeMessageInterface
 {
+    /**
+     * Returns a message to display to the website user
+     *
+     * @return string
+     */
+    public function getPublicMessage()
+    {
+        return 'Your payment was rejected.  ' . $this->getMessage();
+    }
 }
