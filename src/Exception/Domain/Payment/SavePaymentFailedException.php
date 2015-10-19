63b898bc6cb2530cf4b8b035e86c3b803480e27d
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-19 13:26:23 +0100

    Made SavePaymentFailed exception publicly safe

--- src/Exception/Domain/Payment/SavePaymentFailedException.php
+++ src/Exception/Domain/Payment/SavePaymentFailedException.php
@@ -8,11 +8,22 @@
 
 namespace CubicMushroom\Payments\Stripe\Exception\Domain\Payment;
 
+use CubicMushroom\Payments\Stripe\Exception\PublicSafeMessageInterface;
+
 /**
  * Exception throw when failed to save a successful payment
  *
  * @package CubicMushroom\Payments\Stripe
  */
-class SavePaymentFailedException extends AbstractPaymentException
+class SavePaymentFailedException extends AbstractPaymentException implements PublicSafeMessageInterface
 {
+    /**
+     * Returns a message to display to the website user
+     *
+     * @return string
+     */
+    public function getPublicMessage()
+    {
+        return 'cm_payments.exception.save_payment_failed';
+    }
 }
